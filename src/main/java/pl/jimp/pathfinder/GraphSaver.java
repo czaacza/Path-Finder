999a0a7dbfc4374db953090391437b8fef31cfad
Mati Czarnecki <maticzacza@gmail.com>
2022-05-04 17:50:43 +0200

    GraphSaver class created - in progress

---
+++ src/main/java/pl/jimp/pathfinder/GraphSaver.java
@@ -0,0 +1,20 @@
+package pl.jimp.pathfinder;
+
+import java.io.File;
+
+public class GraphSaver {
+    Graph graph;
+    String outputFileName;
+
+    public GraphSaver(Graph graph, String outputFileName) {
+        this.graph = graph;
+        this.outputFileName = outputFileName;
+    }
+
+    public InfoLabel saveGraph(){
+        File outputFile = new File(outputFileName);
+
+
+    return null;
+    }
+}
