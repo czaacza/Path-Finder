9037c63f5dd063be128eac4fc717d09e045de4e3
Mati Czarnecki <maticzacza@gmail.com>
2022-05-23 19:16:32 +0200

    added javadoc

---
+++ specification/doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"pl.jimp.pathfinder"}];updateSearchResults();
---
+++ specification/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"pl.jimp.pathfinder","l":"pl.jimp.pathfinder"}];updateSearchResults();
